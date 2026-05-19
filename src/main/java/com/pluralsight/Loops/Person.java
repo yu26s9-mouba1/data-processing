b6356599b33204b56d9eb263fcec11169f070173
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-19 15:12:32 -0400

    Initial Files

---
+++ src/main/java/com/pluralsight/Loops/Person.java
@@ -0,0 +1,16 @@
+package com.pluralsight.Loops;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+}
---
+++ src/main/java/com/pluralsight/Loops/program.java
@@ -0,0 +1,8 @@
+package com.pluralsight.Loops;
+
+public class program {
+    public static void main(String[] args) {
+
+        
+    }
+}
